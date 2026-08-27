f33ad2d2a204c04b3c59b776ede8a798e1f281b9
vithuvithurshanan <wwwvithu0001@gmail.com>
2026-08-27 11:58:16 -0400

    create dist

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/orchard-park-tree-service/',
+  base: '/',
   plugins: [
     tailwindcss(),
     react(),
